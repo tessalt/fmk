260a1c7674ed423c82a4dc4a7b23110700032ad7
Tessa Thornton <tessathornton@gmail.com>
2017-04-25 18:53:32 -0400

    all corses for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,7 +86,7 @@ Rails.application.configure do
 
   config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://'
+      origins '*'
       resource '*', :headers => :any, :methods => [:get, :post, :options]
     end
   end
